b7518362fcfd6705ce6548c97462d7c1ea3c4776
Aleksandr Hovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-06-08 11:16:25 -0400

    Add collapsible navbar

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function autoTransitionToNightMode(){
 
 // Toggles the night mode label so the text matches the mode the user is in
 function toggleNightModeLabel(){
-    if($('.switch').hasClass('night')) {
-        $('.switch-container span').html('Dark mode');
+    if($('.nightmode-switch').hasClass('night')) {
+        $('.nightmode-switch-container span').html('Dark mode');
     }
     else {
-        $('.switch-container span').html('Light mode');
+        $('.nightmode-switch-container span').html('Light mode');
     }
 }
 
@@ -23,12 +23,13 @@ $(document).ready(function(){
     autoTransitionToNightMode();
 
     // Give the user the option of transitioning between day/night manually
-    $('.switch').click(function(){
+    $('.nightmode-switch').click(function(){
         
         $(document.documentElement).toggleClass('night');
-        $('.switch').toggleClass('night');
+        $('.nightmode-switch').toggleClass('night');
         toggleNightModeLabel();
-    })
+    });
+
 
     // Handles the logic for the collapsibles in the education section
     $('.collapsible').click(function(){
@@ -45,6 +46,22 @@ $(document).ready(function(){
         }
 
         this.scrollIntoView();
+    });
+
+
+    // Toggling navigation bar on mobile
+    $('#topnav .navbar-toggle').click(function(){
+        $('#topnav .nav-links').toggleClass('active');
+    });
+
+
+    // Hide the mobile navbar dropdown after a link is clicked
+    $('.nav-links a').click(function() {
+        var navbarToggler = $('.navbar-toggle');
+
+        if(navbarToggler.css('display') != 'none'){
+            navbarToggler.click();
+        }
     })
 
 });
