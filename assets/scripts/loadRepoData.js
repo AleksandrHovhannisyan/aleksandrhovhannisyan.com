39427a9eed0abb83be98946d5b586474da55bace
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-09-01 12:55:33 -0400

    Add external link icon as SVG

--- assets/scripts/loadRepoData.js
+++ assets/scripts/loadRepoData.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const xmlns = 'http://www.w3.org/2000/svg';
 
 // Using a map instead of an object because I want to show the repos in the order in which they're inserted
 const repos = new Map();
@@ -142,7 +142,6 @@ function nameLabelFor(repo) {
  */
 function stargazerLabelFor(repo) {
     const projectRating = document.createElement('span');
-    const xmlns = 'http://www.w3.org/2000/svg';
     
     const starIcon = document.createElementNS(xmlns, 'svg');
     starIcon.classList.add('star', 'star-filled');
@@ -215,8 +214,11 @@ function createHoverContent() {
     const boldText = document.createElement('strong');
     boldText.textContent = 'View on GitHub';
 
-    const externalLinkIcon = document.createElement('i');
-    externalLinkIcon.classList.add('fas', 'fa-external-link-alt');
+    const externalLinkIcon = document.createElementNS(xmlns, 'svg');
+    const svgPath = document.createElementNS(xmlns, 'path');
+    svgPath.setAttributeNS(null, 'd', 'M1408 928v320q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h704q14 0 23 9t9 23v64q0 14-9 23t-23 9h-704q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113v-320q0-14 9-23t23-9h64q14 0 23 9t9 23zm384-864v512q0 26-19 45t-45 19-45-19l-176-176-652 652q-10 10-23 10t-23-10l-114-114q-10-10-10-23t10-23l652-652-176-176q-19-19-19-45t19-45 45-19h512q26 0 45 19t19 45z');
+    externalLinkIcon.setAttributeNS(null, 'viewBox', '0 0 1792 1792');
+    externalLinkIcon.appendChild(svgPath);
     
     hoverContent.appendChild(boldText);
     hoverContent.appendChild(externalLinkIcon);
