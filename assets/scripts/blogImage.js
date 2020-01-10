bc7a9292687a838ea1fad2615f5acdcdb385950d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-01-10 09:30:52 -0500

    Add lazy loading for blog post images

---
+++ assets/scripts/blogImage.js
@@ -0,0 +1,34 @@
+// Note that img is either a source or img tag
+function preloadImage(img) {
+  if (img.tagName.toLowerCase() === 'source') {
+    img.srcset = img.getAttribute('data-src');
+  } else {
+    img.src = img.getAttribute('data-src');
+  }
+}
+
+const intersectionConfig = {
+  rootMargin: '0px 0px 50px 0px',
+  threshold: 0
+};
+
+const observer = new IntersectionObserver(function(entries, self) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      preloadImage(entry.target);
+      self.unobserve(entry.target);
+    }
+  });
+}, intersectionConfig);
+
+const imgs = document.querySelectorAll('#post [data-src]');
+
+imgs.forEach(img => {
+  // Listen for any intersections to lazy-load
+  observer.observe(img);
+  // Open the image file itself if it's clicked
+  img.addEventListener('click', clickEvent => {
+    const src = clickEvent.target.src;
+    window.open(src, '_self');
+  });
+});
--- assets/scripts/viewImage.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('#post img').forEach(img => {
-  img.addEventListener('click', clickEvent => {
-    const src = clickEvent.target.src;
-    window.open(src, '_self');
-  });
-});
