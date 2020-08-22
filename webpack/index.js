ee9488856fa3f94f1f02fbf5ad59231c83b68a41
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-08-22 15:14:22 -0400

    Fix missing latex build error

--- webpack/index.js
+++ webpack/index.js
@@ -45,3 +45,18 @@ imgs.forEach((img) => {
 document.querySelectorAll('.copy-code-button').forEach((copyCodeButton) => {
   copyCodeButton.addEventListener('click', copyCode);
 });
+
+// Katex/Latex blog posts
+document.querySelectorAll("script[type='math/tex']").forEach((inlineLatex) => {
+  const inlineEquationElement = document.createElement('span');
+  inlineEquationElement.className = 'inline-equation';
+  inlineEquationElement.innerHTML = katex.renderToString(inlineLatex.textContent);
+  inlineLatex.replaceWith(inlineEquationElement);
+});
+
+document.querySelectorAll("script[type='math/tex; mode=display']").forEach((blockLatex) => {
+  const blockEquationElement = document.createElement('div');
+  blockEquationElement.className = 'block-equation';
+  blockEquationElement.innerHTML = katex.renderToString('\\displaystyle ' + blockLatex.textContent);
+  blockLatex.replaceWith(blockEquationElement);
+});
