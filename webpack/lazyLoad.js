4d0bb68638a33145de89424b1268b1045d4d75a8
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-08-23 11:20:41 -0400

    Wrap clickable images in anchors instead of using JS hacks

--- webpack/lazyLoad.js
+++ webpack/lazyLoad.js
@@ -3,32 +3,20 @@
 function lazyLoad(placeholderImg) {
   const pictureElement = placeholderImg.parentElement;
 
-  // Create a new img element and add it to the picture element
+  // Replace fuzzy placeholder with the real image
   const realImg = new Image();
   pictureElement.appendChild(realImg);
   realImg.classList.add('real-image');
   realImg.src = placeholderImg.getAttribute('data-src');
   realImg.alt = placeholderImg.getAttribute('alt');
 
-  // Update the source element to contain the true image (in WebP format)
+  // Replace fuzzy placeholder's webp source with the real image's WebP variant
   const sourceElement = pictureElement.querySelector('source');
   sourceElement.srcset = sourceElement.getAttribute('data-src');
 
   realImg.onload = function () {
-    const isClickable = pictureElement.hasAttribute('data-clickable');
-
-    if (isClickable) {
-      // Open the image if it's clicked
-      realImg.addEventListener('click', (clickEvent) => {
-        const src = clickEvent.target.src;
-        window.open(src, '_self');
-      });
-    }
-
     setTimeout(() => {
-      // We don't need the placeholder anymore, so remove it after a short delay
       placeholderImg.remove();
-      // Need this or else the `picture` will disappear
       realImg.style.position = 'relative';
     }, 500);
   };
