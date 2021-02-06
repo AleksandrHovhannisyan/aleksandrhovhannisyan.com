AleksandrHovhannisyan/aleksandrhovhannisyan.com

commit ef69083966e3f45142cd6cb62de88eaff465b94f

Rework Navbar to not highlight only one link at a time on hover/focus

webpack/components/Navbar/Navbar.js
@@ -1,24 +1,13 @@
-const ACTIVE_LINK_ID = 'active-navbar-link';
-
 export default class Navbar {
   constructor() {
     this.navbar = document.querySelector('.navbar');
     this.menu = this.navbar.querySelector('.navbar-menu');
-    this.links = this.menu.querySelector('.navbar-links');
     this.mobileNavbarToggle = this.navbar.querySelector('#navbar-toggle');
-    this.cachedActiveNavlink = document.getElementById(ACTIVE_LINK_ID);
     this.registerListeners();
   }
 
   registerListeners() {
     this.menu.addEventListener('click', () => this.closeMenu());
-    this.menu.querySelector('.navbar-links').addEventListener('click', (e) => e.stopPropagation());
-    this.links.querySelectorAll('.navbar-link').forEach((anchor) => {
-      anchor.addEventListener('focusin', (e) => this.changeActiveLinkOnHover(e));
-      anchor.addEventListener('mouseenter', (e) => this.changeActiveLinkOnHover(e));
-      anchor.addEventListener('focusout', () => this.restorePreviouslyActiveLink());
-      anchor.addEventListener('mouseleave', () => this.restorePreviouslyActiveLink());
-    });
     this.mobileNavbarToggle.addEventListener('click', () => this.toggleMobileMenu());
   }
 
@@ -42,25 +31,5 @@ export default class Navbar {
   closeMenu() {
     this.navbar.classList.remove('opened');
     this.mobileNavbarToggle.setAttribute('aria-label', 'Open main navigation menu');
-  }
-
-  changeActiveLinkOnHover(mouseEvent) {
-    // Happens if we're on the home page
-    if (!this.cachedActiveNavlink) return;
-
-    const activeNavLink = document.getElementById(ACTIVE_LINK_ID);
-    if (!activeNavLink) return;
-
-    const hoveredAnchor = mouseEvent.target;
-    if (hoveredAnchor === activeNavLink) return;
-
-    this.cachedActiveNavlink = activeNavLink;
-    activeNavLink.id = '';
-  }
-
-  restorePreviouslyActiveLink() {
-    if (this.cachedActiveNavlink) {
-      this.cachedActiveNavlink.id = ACTIVE_LINK_ID;
-    }
   }
 }

commit eed1756efc641a31b57b46ad4e7ba9338666518f

Fix bug caused by ef69083

webpack/components/Navbar/Navbar.js
@@ -2,12 +2,14 @@ export default class Navbar {
   constructor() {
     this.navbar = document.querySelector('.navbar');
     this.menu = this.navbar.querySelector('.navbar-menu');
+    this.links = this.menu.querySelector('.navbar-links');
     this.mobileNavbarToggle = this.navbar.querySelector('#navbar-toggle');
     this.registerListeners();
   }
 
   registerListeners() {
     this.menu.addEventListener('click', () => this.closeMenu());
+    this.links.addEventListener('click', (e) => e.stopPropagation());
     this.mobileNavbarToggle.addEventListener('click', () => this.toggleMobileMenu());
   }
 