29798d78e0b4be20c55f082dff897726b185083d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-07-04 15:25:43 -0400

    Simplify site description

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,8 +2,7 @@ module.exports = {
   title: 'Aleksandr Hovhannisyan',
   author: 'Aleksandr Hovhannisyan',
   email: 'aleksandrhovhannisyan@gmail.com',
-  description:
-    "Hey there! I'm Aleksandr. I write dev tutorials, share my thoughts on software development, and publish the occasional off-topic post.",
+  description: 'Programming tutorials, thoughts on software development, and the occasional off-topic post.',
   keywords: ['Aleksandr Hovhannisyan'],
   // FIXME: don't hardcode port
   url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:4001' : 'https://www.aleksandrhovhannisyan.com',
