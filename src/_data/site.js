f7e375a87bc75b3bddc96e603012ec85a1a1cfbc
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-07-06 18:17:22 -0400

    Use better font scale/hierarchy

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Aleksandr Hovhannisyan',
   author: 'Aleksandr Hovhannisyan',
   email: 'aleksandrhovhannisyan@gmail.com',
-  description: 'Programming tutorials, thoughts on software development, and the occasional off-topic post.',
+  description: 'Dev tutorials, thoughts on software development, and the occasional off-topic post.',
   keywords: ['Aleksandr Hovhannisyan'],
   // FIXME: don't hardcode port
   url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:4001' : 'https://www.aleksandrhovhannisyan.com',
