c07c32f7ad18203274ca87af90e44af6d09e9544
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-08-03 18:31:23 -0400

    Clean up collections code

--- config/collections/index.js
+++ config/collections/index.js
@@ -17,31 +17,25 @@ const categories = (collection) => {
 // Blog posts by category, for pagination
 // Credit: https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/
 const postsByCategory = (collection) => {
-  const itemsPerPage = 20;
+  const postsPerPage = 20;
   const blogPostsByCategory = [];
 
   const allPosts = getAllPosts(collection).reverse();
   const allUniqueCategories = getAllUniqueKeyValues(allPosts, 'categories');
 
-  // Get an array of all posts in this category
   allUniqueCategories.forEach((category) => {
-    const postsInThisCategory = allPosts.filter((post) => {
-      const postCategories = post.data.categories ?? [];
-      return postCategories.includes(category);
-    });
+    // Get all posts belonging to this category
+    const categoryPosts = allPosts.filter((post) => post.data.categories?.includes(category));
 
-    // create a 2D array of chunked posts, where each nested array represents a page.
-    // e.g., if itemsPerPage = 2 and we have three posts, then we'd get [[{post1}, {post2}], [{post3}]]
-    const chunkedPostsInCategory = lodash.chunk(postsInThisCategory, itemsPerPage);
+    // Create a 2D array of chunked posts, where each nested array represents a page.
+    // e.g., if postsPerPage = 2 and we have three posts, then we'd get [[{post1}, {post2}], [{post3}]]
+    const chunkedCategoryPosts = lodash.chunk(categoryPosts, postsPerPage);
 
     // Map each chunk to its page slug
-    const pageSlugs = chunkedPostsInCategory.map((_, i) => {
-      const categorySlug = slugifyString(category);
-      const pageSlug = i > 0 ? `${categorySlug}/page/${i + 1}` : categorySlug;
-      return pageSlug;
-    });
+    const categorySlug = slugifyString(category);
+    const pageSlugs = chunkedCategoryPosts.map((_, i) => i > 0 ? `${categorySlug}/page/${i + 1}` : categorySlug);
 
-    chunkedPostsInCategory.forEach((posts, index) => {
+    chunkedCategoryPosts.forEach((posts, index) => {
       // Massage the data into a format that 11ty's pagination will like
       // https://github.com/11ty/eleventy/issues/332#issuecomment-445236776
       blogPostsByCategory.push({
