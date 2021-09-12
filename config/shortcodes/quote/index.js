827f84d93c51e37bade07b0b8ffa55fabe9cd01e
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-09-12 15:59:09 -0400

    Throw errors from aside and quote shortcodes if inputs are invalid

--- config/shortcodes/quote/index.js
+++ config/shortcodes/quote/index.js
@@ -4,6 +4,15 @@ const { newlineToBr } = require('../../filters');
 const markdownLib = require('../../plugins/markdown');
 
 const quote = (children, source, cite) => {
+  if (!/https?:\/\//.test(cite)) {
+    throw new Error(`Quote citation is not a recognized url: ${cite}.`);
+  }
+  if (!source) {
+    throw new Error('Quotes must attribute a source.');
+  }
+  if (!children) {
+    throw new Error('Quotes must have non-empty content.');
+  }
   const content = markdownLib.renderInline(newlineToBr(children.trim()));
   return outdent`<blockquote cite="${cite}">
     <div class="quote-content">${content}</div>
