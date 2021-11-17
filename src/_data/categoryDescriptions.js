9ed5ae5fadc97719f79f606ca25eeb7fa42f1974
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-11-17 18:09:37 -0500

    Derive categoryDescriptions.all from site.description

---
+++ src/_data/categoryDescriptions.js
@@ -0,0 +1,20 @@
+const site = require('./site');
+
+module.exports = {
+  all: site.description,
+  'computer-science': 'Articles on computer science fundamentals.',
+  'off-topic':
+    "Miscellaneous posts that don't fit into any particular category. I occasionally write about life and other things that interest me.",
+  jekyll: 'Tutorials for Jekyll, a static site generator published as a Ruby gem.',
+  '11ty': 'Articles on 11ty, a highly flexible static site generator written in Node.',
+  math: 'A glimpse into the wonderful world of math.',
+  css: 'Modern CSS is powerful and has come a long way since the early days of web development. Use it well, and you can create stunning experiences on the web.',
+  a11y: 'All kinds of people visit your site. So how do you ensure that your user experience remains... well... <em>usable</em>?',
+  gaming:
+    "When I'm not working or writing, I enjoy playing video games. There's nothing quite like being immersed in a different world!",
+  seo: 'How do you get in front of your target audience and take that coveted number-one spot on Google? With SEO, of course!',
+  react: 'Articles about React, a popular JavaScript framework.',
+  javascript: 'JavaScript tutorials, ranging from vanilla stacks to frameworks.',
+  webperf:
+    'Tutorials on web performance—the art and science of optimizing a website for the best possible user experience, load times, and SEO.',
+};
