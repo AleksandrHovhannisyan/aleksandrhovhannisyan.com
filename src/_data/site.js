0ec9426b4a6801dade2ed4d3e01762f2b9f26c3c
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-11-17 18:13:51 -0500

    Increase md step font size

--- src/_data/site.js
+++ src/_data/site.js
@@ -18,7 +18,7 @@ module.exports = {
   title: 'Aleksandr Hovhannisyan',
   author: 'Aleksandr Hovhannisyan',
   email: 'aleksandrhovhannisyan@gmail.com',
-  description: 'Dev tutorials, thoughts on software development, and the occasional off-topic post.',
+  description: 'Programming tutorials, thoughts on software development, and the occasional off-topic post.',
   keywords: ['Aleksandr Hovhannisyan'],
   issues: {
     owner: `AleksandrHovhannisyan`,
