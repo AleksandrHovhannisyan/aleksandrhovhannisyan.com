12a0a611d49ed949dbb648ca75d7f7cd090df431
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-12-18 11:09:15 -0500

    Simplify ThemeToggle with data-theme instead of classlist

--- src/assets/scripts/components/ThemeToggle/index.mjs
+++ src/assets/scripts/components/ThemeToggle/index.mjs
@@ -5,36 +5,25 @@ export default class ThemeToggle {
     this.toggleElement.addEventListener('click', () => this.toggle());
     this.storageKey = storageKey;
     this.themes = themes;
-    this.theme = localStorage.getItem(storageKey) ?? defaultTheme;
 
-    // On init, only update the class names and sync the aria label. The main
-    // init logic comes from the IIFE in src/_layouts/default.html.
-    this.updateClassNames(defaultTheme, this.theme);
-    this.syncAriaLabelWithStorage();
+    const initialTheme = localStorage.getItem(storageKey) ?? defaultTheme;
+    this.setTheme(initialTheme, false);
   }
 
   get currentTheme() {
-    return this.theme;
+    return this.root.dataset.theme;
   }
 
   get nextTheme() {
-    return this.themes[this.theme];
+    return this.themes[this.currentTheme];
   }
 
-  updateClassNames(oldTheme, newTheme) {
-    this.root.classList.remove(oldTheme);
-    this.root.classList.add(newTheme);
-  }
-
-  syncAriaLabelWithStorage() {
-    this.toggleElement.setAttribute('aria-label', `Switch to ${this.nextTheme} mode theme`);
-  }
-
-  setTheme(newTheme) {
-    this.updateClassNames(this.theme, newTheme);
-    this.theme = newTheme;
-    localStorage.setItem(this.storageKey, newTheme);
-    this.syncAriaLabelWithStorage();
+  setTheme(newTheme, shouldUpdateLocalStorage = true) {
+    this.root.dataset.theme = newTheme;
+    this.toggleElement.setAttribute('aria-label', `Switch to ${this.nextTheme} theme`);
+    if (shouldUpdateLocalStorage) {
+      localStorage.setItem(this.storageKey, newTheme);
+    }
   }
 
   toggle() {
--- src/assets/scripts/components/ThemeToggle/index.test.mjs
+++ src/assets/scripts/components/ThemeToggle/index.test.mjs
@@ -77,38 +77,4 @@ describe('ThemeToggle component', () => {
     toggle.toggle();
     expect(toggle.currentTheme).toEqual('light');
   });
-
-  it("keeps the theme owner's class in sync with the current theme", () => {
-    const toggle = new ThemeToggle({
-      toggleElement,
-      root: document.documentElement,
-      defaultTheme: 'light',
-      themes: {
-        light: 'dark',
-        dark: 'light',
-      },
-    });
-
-    toggle.toggle();
-    expect(document.documentElement.classList.value).toEqual(toggle.currentTheme);
-    toggle.toggle();
-    expect(document.documentElement.classList.value).toEqual(toggle.currentTheme);
-  });
-
-  it("keeps the toggle element's aria-label in sync with the current theme", () => {
-    const toggle = new ThemeToggle({
-      toggleElement,
-      root: document.documentElement,
-      storageKey: 'theme',
-      defaultTheme: 'light',
-      themes: {
-        light: 'dark',
-        dark: 'light',
-      },
-    });
-
-    expect(toggleElement.getAttribute('aria-label')).toEqual(`Switch to dark mode theme`);
-    toggle.toggle();
-    expect(toggleElement.getAttribute('aria-label')).toEqual(`Switch to light mode theme`);
-  });
 });
