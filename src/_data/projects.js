9f3021a1af320f835ffdadef61d1b3c0f0cf3e5b
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-12-30 18:42:16 -0500

    Update projects

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,30 +1,40 @@
 const Cache = require('@11ty/eleventy-cache-assets');
+const { imageShortcode } = require('../../config/shortcodes');
 
 // Combination of static + dynamic data
 const repos = {
+  fluidTypeScale: {
+    getIcon: async () => {
+      const icon = await imageShortcode({
+        src: '/assets/images/projects/fluid-type-scale-calculator.png',
+        alt: '',
+        clickable: false,
+        widths: [32],
+      });
+      return icon;
+    },
+    url: 'https://www.fluid-type-scale.com/',
+    repo: 'AleksandrHovhannisyan/fluid-type-scale-calculator',
+    tech: ['11ty', 'slinkity', 'sass', 'react'],
+  },
   scribe: {
-    icon: '📄',
+    getIcon: () => '📄',
     repo: 'AleksandrHovhannisyan/Scribe-Text-Editor',
     tech: ['c++', 'qt5', 'qtcreator'],
   },
-  cattlelog: {
-    icon: '🐮',
-    repo: 'Camoen/CattleLog',
-    tech: ['android', 'kotlin', 'sql'],
-  },
   usGunCrimes: {
-    icon: '⚖️',
+    getIcon: () => '⚖️',
     repo: 'CIS4301-Project-University-of-Florida/U.S.-Gun-Crime',
     tech: ['react', 'typescript', 'express', 'sql'],
   },
   blog: {
-    icon: '💾',
+    getIcon: () => '💾',
     name: 'This Website!',
     repo: 'AleksandrHovhannisyan/aleksandrhovhannisyan.com',
     tech: ['11ty', 'sass', 'javascript'],
   },
   embody: {
-    icon: '👻',
+    getIcon: () => '👻',
     repo: 'cap4053-cheeky-pixels/EmbodyGame',
     tech: ['c#', 'unity', 'game-ai'],
   },
@@ -32,16 +42,17 @@ const repos = {
 
 const fetchRepo = async (repoKey) => {
   const staticConfig = repos[repoKey];
+  const icon = await staticConfig.getIcon();
   const data = await Cache(`https://api.github.com/repos/${staticConfig.repo}`, {
     duration: '1d',
     type: 'json',
   });
   return {
-    icon: staticConfig.icon,
+    icon,
     name: staticConfig.name ?? data.name,
     rating: data.stargazers_count,
     description: data.description.endsWith('.') ? data.description : `${data.description}.`,
-    url: data.html_url,
+    url: staticConfig.url ?? data.html_url,
     tech: staticConfig.tech,
   };
 };
