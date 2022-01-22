63fe2d1f4089c59998320fc36dce76cc3d0a9543
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-01-22 15:42:35 -0500

    Fix bug where focus indicator was not visible for linked images

--- config/shortcodes/image/index.js
+++ config/shortcodes/image/index.js
@@ -113,7 +113,7 @@ const imageShortcode = async (props) => {
 
   // Link to the highest resolution optimized image
   if (clickable) {
-    return outdent`<a href="${formatSizes[optimizedFormats[0]].largest.url}">${picture}</a>`;
+    return outdent`<a class="image-link" href="${formatSizes[optimizedFormats[0]].largest.url}">${picture}</a>`;
   }
 
   // Otherwise just return the plain picture tag
