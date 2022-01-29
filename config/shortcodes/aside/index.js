b2e4162f82ee461b51af3ab4d0a5f30a359ef397
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-01-29 17:29:05 -0500

    Add role=note to aside component

--- config/shortcodes/aside/index.js
+++ config/shortcodes/aside/index.js
@@ -6,7 +6,7 @@ const aside = (children) => {
     throw new Error('You must provide a non-empty string for an aside.');
   }
   const content = markdownLib.render(children);
-  return `<aside class="post-aside rhythm">${outdent`${content}`}</aside>`;
+  return `<aside role="note" class="post-aside rhythm">${outdent`${content}`}</aside>`;
 };
 
 module.exports = aside;
