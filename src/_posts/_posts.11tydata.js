002e73c37912b1c783ff448ba55ab69f49dd2f33
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-05-14 14:06:51 -0400

    Simplify post ogImage path parsing logic

--- src/_posts/_posts.11tydata.js
+++ src/_posts/_posts.11tydata.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { dir } = require('../../config/constants');
 const { toAbsoluteImageUrl } = require('../../config/filters');
 
 module.exports = {
@@ -11,10 +10,10 @@ module.exports = {
     // For social sharing. Used in og:image and twitter:image. Absolute path to the post's thumbnail image.
     // Thumbnails may be remote images (thumbnail.url) or local images (thumbnail as a string path, like ./images/thumbnail.png).
     ogImage: async (data) => {
-      // filePathStem will look like _posts/yyyy-mm-dd-slug/ for a post located under src/_posts/yyyy-mm-dd-slug/index.md
-      const { dir: imgDir } = path.parse(data.page.filePathStem);
+      // inputPath will look like src/_posts/yyyy-mm-dd-slug/ for a post located under src/_posts/yyyy-mm-dd-slug/index.md
+      const { dir: imgDir } = path.parse(data.page.inputPath);
       // either a full URL for remote images or src/_posts/yyyy-mm-dd-slug/images/name.extension for local images
-      const src = data.thumbnail.url ?? path.join(dir.input, imgDir, data.thumbnail);
+      const src = data.thumbnail.url ?? path.join(imgDir, data.thumbnail);
       // for remote images, just the URL; else, a root-relative path to the image (as seen in the img tag's src)
       return toAbsoluteImageUrl(src);
     },
