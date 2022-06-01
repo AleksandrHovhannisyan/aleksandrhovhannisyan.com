49d41eb415d276b2673b90a1f40829be57cbe958
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-06-01 07:24:44 -0400

    Add role and aria-label to hashArt shortcode itself

--- config/shortcodes/hashArt.js
+++ config/shortcodes/hashArt.js
@@ -1,7 +1,7 @@
 const hashArt = (hash) => {
   const bytes = new Uint8Array(Buffer.from(hash, 'hex'));
   const byteArray = Array.from(bytes).slice(0, bytes.length - 1);
-  let result = `<div class="hash-art-grid">`;
+  let result = `<div class="hash-art-grid" role="img" aria-label="A four-by-four grid of grayscale tiles generated from the hash ${hash}.">`;
   for (let i = 0; i < byteArray.length - 3; i++) {
     const rgb = byteArray.slice(i, i + 3);
     result += `<div class="hash-art-cell" style="background-color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})"></div>`;
