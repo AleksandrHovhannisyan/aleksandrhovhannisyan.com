dc12f65d4be1925af3900e6d7d27ce2624759348
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-06-06 17:54:28 -0400

    Use fontkit to read font file postscript names

--- src/_data/fonts.js
+++ src/_data/fonts.js
@@ -1,21 +1,14 @@
 const path = require('path');
+const { dir } = require('../../config/constants');
 const fontPath = `/assets/fonts`;
+const fontkit = require('fontkit');
 
 const FontWeight = {
   REGULAR: 'regular',
+  REGULARITALIC: 'italic',
   MEDIUM: 'medium',
   BOLD: 'bold',
   EXTRABOLD: 'extraBold',
-  ITALIC: 'italic',
-};
-
-const FontVariant = {
-  LIGHT: 'Light',
-  REGULAR: 'Regular',
-  MEDIUM: 'Medium',
-  BOLD: 'Bold',
-  EXTRABOLD: 'ExtraBold',
-  ITALIC: 'Italic',
 };
 
 const FontStyle = {
@@ -37,21 +30,18 @@ const fonts = {
     fallbacks: [`Rubik-fallback`],
     weights: {
       [FontWeight.REGULAR]: {
-        variant: FontVariant.REGULAR,
         weight: 400,
         style: FontStyle.NORMAL,
         url: getFontUrl('rubik-latin-400.woff2'),
         display: FontDisplay.SWAP,
       },
-      [FontWeight.ITALIC]: {
-        variant: FontVariant.ITALIC,
+      [FontWeight.REGULARITALIC]: {
         weight: 400,
         style: FontStyle.ITALIC,
         url: getFontUrl('rubik-latin-italic.woff2'),
         display: FontDisplay.SWAP,
       },
       [FontWeight.BOLD]: {
-        variant: FontVariant.BOLD,
         weight: 700,
         style: FontStyle.NORMAL,
         url: getFontUrl('rubik-latin-700.woff2'),
@@ -87,21 +77,18 @@ const fonts = {
     fallbacks: [`Monaco`, `Consolas`, `Courier New`, `monospace`],
     weights: {
       [FontWeight.REGULAR]: {
-        variant: FontVariant.REGULAR,
         weight: 400,
         style: FontStyle.NORMAL,
         url: getFontUrl('ibm-plex-mono-v7-latin-regular.woff2'),
         display: FontDisplay.SWAP,
       },
       [FontWeight.MEDIUM]: {
-        variant: FontVariant.MEDIUM,
         weight: 500,
         style: FontStyle.NORMAL,
         url: getFontUrl('ibm-plex-mono-v7-latin-500.woff2'),
         display: FontDisplay.SWAP,
       },
       [FontWeight.BOLD]: {
-        variant: FontVariant.BOLD,
         weight: 700,
         style: FontStyle.NORMAL,
         url: getFontUrl('ibm-plex-mono-v7-latin-700.woff2'),
@@ -114,7 +101,6 @@ const fonts = {
     fallbacks: [`cursive`],
     weights: {
       [FontWeight.REGULAR]: {
-        variant: FontVariant.REGULAR,
         weight: 400,
         style: FontStyle.NORMAL,
         url: getFontUrl('reenie-beanie-v11-latin-regular.woff2'),
@@ -124,4 +110,24 @@ const fonts = {
   },
 };
 
-module.exports = fonts;
+/** Injects poscript names into each font config definition */
+const withPostscriptNames = (fontConfig) => {
+  // No need to clone deep
+  const enhancedFonts = Object.assign({}, fontConfig);
+  Object.values(enhancedFonts).forEach((font) => {
+    // Variable fonts don't have individual variant configs, so inject in the top-level object
+    if (font.isVariable) {
+      const fontFile = fontkit.openSync(path.join(dir.input, font.url));
+      font.postscriptName = fontFile.postscriptName;
+    } else {
+      // Non-variable fonts need one postscript name per weight variant
+      Object.values(font.weights).forEach((weight) => {
+        const fontFile = fontkit.openSync(path.join(dir.input, weight.url));
+        weight.postscriptName = fontFile.postscriptName;
+      });
+    }
+  });
+  return enhancedFonts;
+};
+
+module.exports = withPostscriptNames(fonts);
