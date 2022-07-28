50b2611d4ecbf9db53306c039a6a999ee4d7b095
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-07-28 08:20:44 -0400

    Split default.html into base.html layout

--- src/assets/scripts/index.mjs
+++ src/assets/scripts/index.mjs
@@ -2,23 +2,26 @@ import { THEME_KEY, Themes, copyToClipboardButtonStrings } from './constants.mjs
 import ThemeToggle from './components/ThemeToggle.mjs';
 
 const themeToggleElement = document.getElementById('theme-toggle');
-const cachedTheme = localStorage.getItem(THEME_KEY);
-const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? Themes.DARK : Themes.LIGHT;
 
-// eslint-disable-next-line no-unused-vars
-const themeToggle = new ThemeToggle({
-  toggleElement: themeToggleElement,
-  initialTheme: cachedTheme ?? preferredTheme,
-  setTheme: (theme) => {
-    document.documentElement.dataset[THEME_KEY] = theme;
-    themeToggleElement.setAttribute('aria-pressed', theme === Themes.DARK);
-  },
-  setCachedTheme: (theme) => localStorage.setItem(THEME_KEY, theme),
-  themes: {
-    [Themes.LIGHT]: Themes.DARK,
-    [Themes.DARK]: Themes.LIGHT,
-  },
-});
+if (themeToggleElement) {
+  const cachedTheme = localStorage.getItem(THEME_KEY);
+  const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? Themes.DARK : Themes.LIGHT;
+
+  // eslint-disable-next-line no-unused-vars
+  const themeToggle = new ThemeToggle({
+    toggleElement: themeToggleElement,
+    initialTheme: cachedTheme ?? preferredTheme,
+    setTheme: (theme) => {
+      document.documentElement.dataset[THEME_KEY] = theme;
+      themeToggleElement.setAttribute('aria-pressed', theme === Themes.DARK);
+    },
+    setCachedTheme: (theme) => localStorage.setItem(THEME_KEY, theme),
+    themes: {
+      [Themes.LIGHT]: Themes.DARK,
+      [Themes.DARK]: Themes.LIGHT,
+    },
+  });
+}
 
 const copyableCodeBlocks = document.querySelectorAll('code[data-copyable="true"]');
 copyableCodeBlocks.forEach((codeBlock) => {
