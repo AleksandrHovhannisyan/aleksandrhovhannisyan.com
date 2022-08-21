e29bfb41080d4caba56b01e7fd278963c2ab52c1
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-08-21 18:44:08 -0400

    Fix bug where only copyable code blocks would get a tabindex

--- src/assets/scripts/index.mjs
+++ src/assets/scripts/index.mjs
@@ -23,35 +23,37 @@ if (themeToggleElement) {
   });
 }
 
-const copyableCodeBlocks = document.querySelectorAll('code[data-copyable="true"]');
-copyableCodeBlocks.forEach((codeBlock) => {
+document.querySelectorAll('code[class*=language-]').forEach((codeBlock) => {
   // TODO: This is my only option until https://github.com/11ty/eleventy-plugin-syntaxhighlight/issues/72 is addressed
   codeBlock.setAttribute('tabindex', 0);
 
-  const code = codeBlock.innerText;
-  const copyCodeButton = document.createElement('button');
-  copyCodeButton.className = 'copy-code-button fs-sm';
-  copyCodeButton.innerText = copyToClipboardButtonStrings.default;
-  copyCodeButton.setAttribute('aria-label', copyToClipboardButtonStrings.ariaLabel);
-  copyCodeButton.type = 'button';
-  codeBlock.parentElement.append(copyCodeButton);
+  if (codeBlock.getAttribute('data-copyable')) {
+    const code = codeBlock.innerText;
+    const copyCodeButton = document.createElement('button');
+    copyCodeButton.className = 'copy-code-button fs-sm';
+    copyCodeButton.innerText = copyToClipboardButtonStrings.default;
+    // Set an aria label explicitly to clarify the button's action a bit better for screen reader users; sighted users should be able to relate "Copy" to the code block in which the button is positioned
+    copyCodeButton.setAttribute('aria-label', copyToClipboardButtonStrings.ariaLabel);
+    copyCodeButton.type = 'button';
+    codeBlock.parentElement.append(copyCodeButton);
 
-  // Accessible alert whose inner text changes when we copy.
-  const copiedAlert = document.createElement('span');
-  copiedAlert.setAttribute('role', 'alert');
-  copiedAlert.classList.add('screen-reader-only');
-  codeBlock.parentElement.append(copiedAlert);
+    // Accessible alert whose inner text changes when we copy.
+    const copiedAlert = document.createElement('span');
+    copiedAlert.setAttribute('role', 'alert');
+    copiedAlert.classList.add('screen-reader-only');
+    codeBlock.parentElement.append(copiedAlert);
 
-  copyCodeButton.addEventListener('click', () => {
-    window.navigator.clipboard.writeText(code);
-    copyCodeButton.classList.add('copied');
-    copyCodeButton.innerText = copyToClipboardButtonStrings.copied;
-    copiedAlert.innerText = copyToClipboardButtonStrings.copied;
+    copyCodeButton.addEventListener('click', () => {
+      window.navigator.clipboard.writeText(code);
+      copyCodeButton.classList.add('copied');
+      copyCodeButton.innerText = copyToClipboardButtonStrings.copied;
+      copiedAlert.innerText = copyToClipboardButtonStrings.copied;
 
-    setTimeout(() => {
-      copyCodeButton.classList.remove('copied');
-      copyCodeButton.innerText = copyToClipboardButtonStrings.default;
-      copiedAlert.innerText = '';
-    }, 2000);
-  });
+      setTimeout(() => {
+        copyCodeButton.classList.remove('copied');
+        copyCodeButton.innerText = copyToClipboardButtonStrings.default;
+        copiedAlert.innerText = '';
+      }, 2000);
+    });
+  }
 });
