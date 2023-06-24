90f61cbe2ce1935f78ea236513a6b083f83453d8
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2023-06-24 15:19:01 -0500

    Update meta description

--- src/_data/site.js
+++ src/_data/site.js
@@ -12,7 +12,7 @@ module.exports = {
   title: 'Aleksandr Hovhannisyan',
   author: 'Aleksandr Hovhannisyan',
   email: 'aleksandrhovhannisyan@gmail.com',
-  description: 'Dev tutorials, thoughts on software development, and the occasional off-topic post.',
+  description: 'Dev tutorials, thoughts on software development, and the occasional essay.',
   keywords: ['Aleksandr Hovhannisyan'],
   lang: 'en-US',
   issues: {
