fcda63d9008566602f79dd17f6b4196b6efe3423
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-11-08 12:01:25 -0600

    chore: remove post chunking/pagination

--- packages/web/lib/collections.js
+++ packages/web/lib/collections.js
@@ -1,28 +1,26 @@
-import chunk from 'lodash/chunk.js';
 import { slugifyString } from './utils.js';
 import { dir } from './constants.js';
 
 /** Returns all blog posts as a collection. */
-export const getAllPosts = (collection) => {
+export function getAllPosts(collection) {
   const posts = collection.getFilteredByGlob(`${dir.input}/_posts/**/*.md`);
   return posts.reverse().filter((post) => !post.data.isArchived);
-};
+}
 
-/** Given a category name and an optional page number, returns the root-relative URL to that category's page.
+/** Given a category name, returns the root-relative URL to that category's page.
  * @param {string} category
- * @param {number} [page] The one-indexed page number
  */
-export const getCategoryHref = (category, page) => {
+function getCategoryHref(category) {
   const slug = slugifyString(category);
-  return page > 1 ? `/tags/${slug}/page/${page}/` : `/tags/${slug}/`;
-};
+  return `/tags/${slug}/`;
+}
 
 /** Returns all unique categories as a collection.
  * NOTE: I'm calling these "categories" to distinguish them from 11ty's built-in "tags." However,
  * these are still referred to as tags in the UI since that's what's most common.
  * @returns {({ title: string; href: string; count: string })[]}
  */
-export const getAllUniqueCategories = (collection) => {
+export function getAllUniqueCategories(collection) {
   const allPosts = getAllPosts(collection);
 
   /** @type {Map<string, number>} */
@@ -46,47 +44,20 @@ export const getAllUniqueCategories = (collection) => {
       // And then by popular categories first
       .sort((a, b) => b.count - a.count)
   );
-};
+}
 
 // Blog posts by category, for pagination
 // Adapted for use from: https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/
-export const getPostsByCategory = (collection) => {
-  const postsPerPage = 100;
-  const blogPostsByCategory = [];
-
+export function getPostsByCategory(collection) {
   const allPosts = getAllPosts(collection);
   const allUniqueCategories = getAllUniqueCategories(collection);
 
-  allUniqueCategories.forEach((category) => {
-    // Get all posts belonging to this category
-    const categoryPosts = allPosts.filter((post) => post.data.categories?.includes(category.title));
-
-    // Create a 2D array of chunked posts, where each nested array represents a page.
-    // e.g., if postsPerPage = 2 and we have three posts, then we'd get [[{post1}, {post2}], [{post3}]]
-    const chunkedCategoryPosts = chunk(categoryPosts, postsPerPage);
-
-    // Map each chunk to its page slug
-    const pageHrefs = chunkedCategoryPosts.map((_, pageIndex) => getCategoryHref(category.title, pageIndex + 1));
-
-    chunkedCategoryPosts.forEach((posts, index) => {
-      // Massage the data into a format that 11ty's pagination will like
-      // https://github.com/11ty/eleventy/issues/332#issuecomment-445236776
-      blogPostsByCategory.push({
-        title: category.title,
-        href: pageHrefs[index],
-        pageNumber: index,
-        totalPages: pageHrefs.length,
-        hrefs: {
-          all: pageHrefs,
-          next: pageHrefs[index + 1] || null,
-          previous: pageHrefs[index - 1] || null,
-          first: pageHrefs[0] || null,
-          last: pageHrefs[pageHrefs.length - 1] || null,
-        },
-        posts,
-      });
-    });
-  });
-
+  // Massage the data into a format that 11ty's pagination will like
+  // https://github.com/11ty/eleventy/issues/332#issuecomment-445236776
+  const blogPostsByCategory = allUniqueCategories.map((category) => ({
+    title: category.title,
+    href: getCategoryHref(category.title),
+    posts: allPosts.filter((post) => !!post.data.categories?.includes(category.title)),
+  }));
   return blogPostsByCategory;
-};
+}
