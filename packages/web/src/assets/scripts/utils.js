51902f8d276c8d67c158aa6be15f3d91dcbfa049
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-11-15 18:47:22 -0600

    Fix throttle util

--- packages/web/src/assets/scripts/utils.js
+++ packages/web/src/assets/scripts/utils.js
@@ -1,16 +1,31 @@
 /**
  * Returns a new function that invokes `func` no more than once per `rateMs` milliseconds.
- * @param {(...args: unknown[]) => void} func
- * @param {number} rateMs
+ * @param {(...args: unknown[]) => void} callback The function to call.
+ * @param {number} rateMs At most, `callback` will only ever run once per this period of time.
  */
-export function throttle(func, rateMs) {
-  let lastInvocationTimeMs = null;
+export function throttle(callback, rateMs) {
+  let isWaiting = false;
+  // Args for the most recent invocation that was queued/skipped. When the timer goes off, we want to invoke the callback with these args.
+  let mostRecentlyQueuedArgs = null;
 
-  return function (...args) {
-    const nowMs = Date.now();
-    if (nowMs - lastInvocationTimeMs >= rateMs) {
-      func(...args);
-      lastInvocationTimeMs = nowMs;
+  return (...args) => {
+    // Skip intermediate calls until the timer goes off, but keep track of the args so we don't drop off any calls after the timer.
+    if (isWaiting) {
+      mostRecentlyQueuedArgs = args;
+      return;
     }
+
+    callback(...args);
+    isWaiting = true;
+
+    // After rateMs passes, the next call will be eligible
+    setTimeout(() => {
+      isWaiting = false;
+      // Run the most recently queued function call, if there is one
+      if (mostRecentlyQueuedArgs) {
+        callback(mostRecentlyQueuedArgs);
+        mostRecentlyQueuedArgs = null;
+      }
+    }, rateMs);
   };
 }
