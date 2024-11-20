c277acb6bc987b418764c422b148a1500c49f308
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2024-11-19 18:32:41 -0600

    Clean up gameLoop.js

--- packages/web/src/assets/scripts/demos/gameLoop.js
+++ packages/web/src/assets/scripts/demos/gameLoop.js
@@ -2,10 +2,29 @@ function toRadians(degrees) {
   return (degrees * Math.PI) / 180;
 }
 
-class Vector {
+/** Two-dimensional vector. */
+class Vector2D {
+  /** @type {number} The x-coordinate of the vector in space. */
+  x;
+  /** @type {number} The y-coordinate of the vector in space. */
+  y;
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
   constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
+  }
+
+  /**
+   * Clones the given vector.
+   * @param {Vector2D} vector
+   * @returns {Vector2D}
+   */
+  static from(vector) {
+    return new Vector2D(vector.x, vector.y);
   }
 
   /** Returns the Cartesian length of this vector. */
@@ -14,10 +33,11 @@ class Vector {
   }
 
   /**
+   * Returns a new vector that's the result of scaling this vector by the given scalar.
    * @param {number} scalar
    */
   scaled(scalar) {
-    const scaled = new Vector(this.x, this.y);
+    const scaled = Vector2D.from(this);
     scaled.x *= scalar;
     scaled.y *= scalar;
     return scaled;
@@ -25,7 +45,7 @@ class Vector {
 
   /** Returns a new vector that's the result of normalizing this vector (i.e., a unit vector of length `1` in the same direction). */
   normalized() {
-    const unitVector = new Vector(this.x, this.y);
+    const unitVector = Vector2D.from(this);
     return unitVector.scaled(1 / unitVector.length);
   }
 
@@ -33,51 +53,16 @@ class Vector {
    * @param {number} angleDeltaRadians
    */
   rotated(angleDeltaRadians) {
-    const rotated = new Vector(this.x, this.y);
+    const rotated = Vector2D.from(this);
     rotated.x = this.x * Math.cos(angleDeltaRadians) - this.y * Math.sin(angleDeltaRadians);
     rotated.y = this.x * Math.sin(angleDeltaRadians) + this.y * Math.cos(angleDeltaRadians);
     return rotated;
   }
 }
 
-class Player {
-  /** @type {number} */
-  radius;
-  /** @type {Vector} */
-  position;
-  /** @type {Vector} */
-  direction;
-  /** @type {number} */
-  speed;
-
-  /**
-   *
-   * @param {{ position: Vector; direction: Vector; speed: number; radius: number }} props
-   */
-  constructor({ position, direction, speed, radius }) {
-    this.position = position;
-    this.direction = direction;
-    this.speed = speed;
-    this.radius = radius;
-  }
-
-  /** Moves the player at its current speed in the the specified direction.
-   * @param {Vector} direction
-   */
-  move(direction) {
-    this.position.x += direction.x * this.speed;
-    this.position.y += direction.y * this.speed;
-  }
-
-  /**
-   * Rotates this player by the given change in angle, in radians.
-   * @param {number} angleDeltaRadians
-   */
-  rotate(angleDeltaRadians) {
-    this.direction = this.direction.rotated(angleDeltaRadians);
-  }
-}
-
+/**
+ * Helper around `HTMLCanvasElement`. Pass a canvas to it and it provides methods for drawing.
+ */
 class Canvas {
   /** @type {HTMLCanvasElement} */
   #canvas;
@@ -133,14 +118,50 @@ class Canvas {
     this.#ctx.stroke();
   }
 }
+class Player {
+  /** @type {number} */
+  radius;
+  /** @type {Vector2D} */
+  position;
+  /** @type {Vector2D} */
+  direction;
+  /** @type {number} */
+  speed;
+
+  /**
+   * @param {{ position: Vector2D; direction: Vector2D; speed: number; radius: number }} props
+   */
+  constructor({ position, direction, speed, radius }) {
+    this.position = position;
+    this.direction = direction;
+    this.speed = speed;
+    this.radius = radius;
+  }
+
+  /** Moves the player at its current speed in the the specified direction.
+   * @param {Vector2D} direction
+   */
+  move(direction) {
+    this.position.x += direction.x * this.speed;
+    this.position.y += direction.y * this.speed;
+  }
+
+  /**
+   * Rotates this player by the given change in angle, in radians.
+   * @param {number} angleDeltaRadians
+   */
+  rotate(angleDeltaRadians) {
+    this.direction = this.direction.rotated(angleDeltaRadians);
+  }
+}
 
 class GameLoop extends HTMLElement {
   /** @type {Set<string>} */
-  pressedKeys;
+  #pressedKeys;
   /** @type {Player} */
-  player;
+  #player;
   /** @type {Canvas} */
-  canvas;
+  #canvas;
 
   constructor() {
     super();
@@ -151,10 +172,10 @@ class GameLoop extends HTMLElement {
     canvas.style.width = '100%';
     canvas.width = Number(width);
     canvas.height = Number(height);
-    this.canvas = new Canvas(canvas);
-    this.player = new Player({
-      position: new Vector(width / 2, height / 2),
-      direction: new Vector(1, 0).normalized(),
+    this.#canvas = new Canvas(canvas);
+    this.#player = new Player({
+      position: new Vector2D(width / 2, height / 2),
+      direction: new Vector2D(1, 0).normalized(),
       speed: 4,
       radius: 8,
     });
@@ -163,14 +184,14 @@ class GameLoop extends HTMLElement {
     canvas.setAttribute('tabindex', 0);
     canvas.setAttribute('role', 'region');
     canvas.setAttribute('aria-label', label);
-    const shadow = this.attachShadow({ mode: 'open' });
+    const shadow = this.attachShadow({ mode: 'closed' });
     shadow.appendChild(canvas);
   }
 
   connectedCallback() {
-    this.pressedKeys = new Set();
-    this.addEventListener('keydown', (e) => this.pressedKeys.add(e.key));
-    this.addEventListener('keyup', (e) => this.pressedKeys.delete(e.key));
+    this.#pressedKeys = new Set();
+    this.addEventListener('keydown', (e) => this.#pressedKeys.add(e.key));
+    this.addEventListener('keyup', (e) => this.#pressedKeys.delete(e.key));
     this.#update();
   }
 
@@ -178,39 +199,39 @@ class GameLoop extends HTMLElement {
    * @param {string} key
    */
   #isKeyDown(key) {
-    return this.pressedKeys.has(key);
+    return this.#pressedKeys.has(key);
   }
 
   #checkUserInput() {
-    if (this.#isKeyDown('w')) {
-      this.player.move(this.player.direction);
+    if (this.#isKeyDown('w') || this.#isKeyDown('W')) {
+      this.#player.move(this.#player.direction);
     }
-    if (this.#isKeyDown('s')) {
-      this.player.move(this.player.direction.scaled(-1));
+    if (this.#isKeyDown('s') || this.#isKeyDown('S')) {
+      this.#player.move(this.#player.direction.scaled(-1));
     }
-    if (this.#isKeyDown('a')) {
-      this.player.rotate(toRadians(-4));
+    if (this.#isKeyDown('a') || this.#isKeyDown('A')) {
+      this.#player.rotate(toRadians(-4));
     }
-    if (this.#isKeyDown('d')) {
-      this.player.rotate(toRadians(4));
+    if (this.#isKeyDown('d') || this.#isKeyDown('D')) {
+      this.#player.rotate(toRadians(4));
     }
   }
 
   #draw() {
-    this.canvas.clear();
-    this.canvas.fill('white');
-    this.canvas.circle({
-      x: this.player.position.x,
-      y: this.player.position.y,
-      radius: this.player.radius,
+    this.#canvas.clear();
+    this.#canvas.fill('white');
+    this.#canvas.circle({
+      x: this.#player.position.x,
+      y: this.#player.position.y,
+      radius: this.#player.radius,
       color: 'black',
     });
-    const direction = this.player.direction.scaled(20);
-    this.canvas.line({
-      x1: this.player.position.x,
-      y1: this.player.position.y,
-      x2: this.player.position.x + direction.x,
-      y2: this.player.position.y + direction.y,
+    const direction = this.#player.direction.scaled(20);
+    this.#canvas.line({
+      x1: this.#player.position.x,
+      y1: this.#player.position.y,
+      x2: this.#player.position.x + direction.x,
+      y2: this.#player.position.y + direction.y,
       color: 'black',
       strokeWidth: '2',
     });
