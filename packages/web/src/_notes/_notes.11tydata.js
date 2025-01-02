8bdace93c0a5e4ea256740bf690dc125eb794292
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2025-01-02 16:22:55 -0600

    Revert "Add schema validation to blog posts"
    This reverts commit e98069dc46c669c0a9ac13bb9f93b044d1e99d19.

--- packages/web/src/_notes/_notes.11tydata.js
+++ packages/web/src/_notes/_notes.11tydata.js
@@ -1,7 +1,4 @@
-import { validateBlogPostSchema } from '../_posts/_posts.11tydata.js';
-
 export default {
-  eleventyDataSchema: validateBlogPostSchema,
   layout: 'note',
   tags: ['notes'],
   permalink: (data) => `/notes/${data?.page?.fileSlug}/`,
