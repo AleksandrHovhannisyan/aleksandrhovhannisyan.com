f9ed9945ec09fba14e54845faf6894864f7ccc06
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2025-02-04 18:12:15 -0600

    Remove profile photo og image from pages

--- packages/web/src/_pages/_pages.11tydata.js
+++ packages/web/src/_pages/_pages.11tydata.js
@@ -1,9 +1,3 @@
-import { toAbsoluteImageUrl } from '../../lib/filters.js';
-
 export default {
   layout: 'default',
-  openGraph: {
-    type: 'website',
-    image: toAbsoluteImageUrl({ src: 'src/assets/images/profile-photo.jpg', width: 400 }),
-  },
 };
