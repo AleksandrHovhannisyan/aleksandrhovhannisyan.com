d7e4e28406cacba90ca7bb6bc65b29e327e99632
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2025-02-15 12:11:22 -0600

    Clean up dialog controls

--- packages/web/src/assets/scripts/dialog.js
+++ packages/web/src/assets/scripts/dialog.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('button.dialog-open[aria-controls]').forEach((openButton) => {
+document.querySelectorAll('button[data-dialog-action="show"][aria-controls]').forEach((openButton) => {
   const dialogId = openButton.getAttribute('aria-controls');
   /** @type {HTMLDialogElement} */
   const dialog = document.getElementById(dialogId);
@@ -15,6 +15,10 @@ document.querySelectorAll('button.dialog-open[aria-controls]').forEach((openButt
     openButton.setAttribute('aria-expanded', shouldOpen.toString());
   }
 
+  /** @type {HTMLButtonElement} */
+  const closeButton = dialog.querySelector(`button[data-dialog-action="hide"][aria-controls="${dialogId}"]`);
+  closeButton.addEventListener('click', () => setIsOpen(false));
+
   openButton.addEventListener('click', (e) => {
     // Otherwise the click event will bubble to the outside-click listener registered below
     e.stopPropagation();
@@ -22,10 +26,6 @@ document.querySelectorAll('button.dialog-open[aria-controls]').forEach((openButt
     if (!dialog.open) {
       setIsOpen(true);
     }
-
-    /** @type {HTMLButtonElement} */
-    const closeButton = dialog.querySelector(`button[aria-controls="${dialogId}"]`);
-    closeButton.addEventListener('click', () => setIsOpen(false));
 
     // Detect outside clicks. NOTE: This only works if the ::backdrop ignores pointer events.
     // Once closedby has enough support, remove this https://github.com/whatwg/html/pull/10737
