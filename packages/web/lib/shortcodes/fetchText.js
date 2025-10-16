c6148e6fba85c114467a0e6798745da58bc1a4eb
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2025-10-16 17:58:38 -0500

    Bump dependencies

--- packages/web/lib/shortcodes/fetchText.js
+++ packages/web/lib/shortcodes/fetchText.js
@@ -1,7 +1,7 @@
-import Cache from '@11ty/eleventy-cache-assets';
+import Fetch from '@11ty/eleventy-fetch';
 
-export default async function fetchText(url) {
+export default async function fetchText(url, duration) {
   console.log(`${this.page.url}: Fetching ${url} as plaintext...`);
-  const text = await Cache(url, { duration: '4w', type: 'text' });
+  const text = await Fetch(url, { duration, type: 'text' });
   return text;
 }
